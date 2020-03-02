80ca2ff011e5677e62b84cfda9d894fc97db8017
ieremin2000 <ieremin2000@gmail.com>
2020-03-02 18:26:26 -0500

    changed app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi there 3!
+          Hi there 4!
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
